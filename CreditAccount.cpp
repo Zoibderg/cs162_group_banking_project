5de87641c565103668a0cfed66a83ae1aa0c47af
Austin Tesch <alrtesch@gmail.com>
2024-12-04 13:59:13 -0800

    update CreditAccount
    created header for CreditAccount, added constructor, implemented calculate_min_payment, make_payment and charge which acts like a withdraw, but is exclusive to credit accounts. added comments and documentatnion

--- CreditAccount.cpp
+++ CreditAccount.cpp
@@ -1,18 +1,72 @@
-#include "CreditAccount.h"
 #include "BankAccount.h"
+#include <iostream>
+using namespace std;
 
+/**
+ * CreditAccount class represents a credit account derived from the base BankAccount class.
+ * It provides additional functionality specific to credit accounts, such as managing credit limit, calculating minimum payments, and making charges.
+ */
 class CreditAccount : public BankAccount
 {
 public:
-    double APR;
+    double APR; // Annual Percentage Rate for the credit account
 
-    void calculate_min_payment() // calculates the minimum payment user must make on the credit cardd
+    /**
+     * Constructor to initialize the credit account with account details.
+     * @param accountNumber The unique number of the account.
+     * @param holderName The name of the account holder.
+     * @param initialBalance The initial balance of the account.
+     * @param apr The annual percentage rate for the account.
+     * @param limit The credit limit for the account.
+     */
+    CreditAccount(int accountNumber, const string &holderName, double initialBalance, double apr, double limit)
+        : BankAccount(accountNumber, holderName, "Credit", "Active", initialBalance), APR(apr), credit_limit(limit), current_balance(initialBalance) {}
+
+    /**
+     * Method to calculate the minimum payment required for the credit account.
+     * The minimum payment is assumed to be 5% of the current balance.
+     */
+    void calculate_min_payment() const
     {
+        double min_payment = 0.05 * current_balance;
+        cout << "The minimum payment for this account is: $" << min_payment << endl;
+    }
 
+    /**
+     * Method to make a payment towards the credit balance.
+     * @param amount The amount to be paid. Must be greater than zero.
+     */
+    void make_payment(double amount)
+    {
+        if (amount > 0)
+        {
+            current_balance -= amount;
+            cout << "Payment of $" << amount << " has been made. Current balance: $" << current_balance << endl;
+        }
+        else
+        {
+            cout << "Invalid payment amount." << endl;
+        }
+    }
+
+    /**
+     * Method to charge the credit account (i.e., make a purchase).
+     * @param amount The amount to be charged. Must be greater than zero and not exceed the credit limit.
+     */
+    void charge(double amount)
+    {
+        if (amount > 0 && (current_balance + amount) <= credit_limit)
+        {
+            current_balance += amount;
+            cout << "Charge of $" << amount << " has been added. Current balance: $" << current_balance << endl;
+        }
+        else
+        {
+            cout << "Charge exceeds credit limit or is invalid." << endl;
+        }
     }
 
 private:
-    double credit_limit;
-    double current_balance;
-
+    double credit_limit; // Limit for the credit account
+    double current_balance; // Current balance for the credit account
 };
--- CreditAccount.h
+++ CreditAccount.h
@@ -1,16 +1,31 @@
-//
-// Created by Austin Tesch on 12/4/24.
-//
-
 #ifndef CREDITACCOUNT_H
 #define CREDITACCOUNT_H
 
+#include "BankAccount.h"
+#include <string>
 
+/**
+ * CreditAccount class represents a credit account derived from the base BankAccount class.
+ * It provides additional functionality specific to credit accounts, such as minimum payment calculations.
+ */
+class CreditAccount : public BankAccount
+{
+public:
+    double APR; // Annual Percentage Rate for the credit account
 
-class CreditAccount {
+    /**
+     * Constructor to initialize the credit account with account details.
+     */
+    CreditAccount(int accountNumber, const std::string &holderName, double initialBalance);
 
+    /**
+     * Method to calculate the minimum payment required for the credit account.
+     */
+    void calculate_min_payment();
+
+private:
+    double credit_limit; // Limit for the credit account
+    double current_balance; // Current balance for the credit account
 };
 
-
-
-#endif //CREDITACCOUNT_H
+#endif // CREDITACCOUNT_H
