3a64ab5fcdc84b3317190ccec2f20d1f13e7868c
Austin Tesch <alrtesch@gmail.com>
2024-12-04 13:04:20 -0800

    update CheckingAccount
    completed CheckingAccount header, added constructor, added more comments and documentation

--- CheckingAccount.cpp
+++ CheckingAccount.cpp
@@ -1,32 +1,57 @@
-#include "CheckingAccount.h"
 #include "BankAccount.h"
 #include <iostream>
 using namespace std;
 
+/**
+ * CheckingAccount class represents a checking account derived from the base BankAccount class.
+ * It provides additional functionality specific to checking accounts, such as overdraft checks and fees.
+ */
 class CheckingAccount : public BankAccount
 {
 public:
-    double accountbalance;
-    void check_overdraft() // checks to see if the user is negative in the account
+    double account_balance; // Current balance of the checking account
+
+    /**
+     * Constructor to initialize the checking account with account details.
+     * @param accountNumber The unique number of the account.
+     * @param holderName The name of the account holder.
+     * @param initialBalance The initial balance of the account.
+     */
+    CheckingAccount(int accountNumber, const string &holderName, double initialBalance)
+        : BankAccount(accountNumber, holderName, "Checking", "Active", initialBalance),
+          account_balance(initialBalance) {
+    }
+
+    /**
+     * Method to check if the account balance has gone negative.
+     * If the balance is negative, an overdraft fee will be applied.
+     * If the balance is positive, no fee is applied.
+     */
+    void check_overdraft() const
     {
-        if (accountbalance < 0)
+        if (account_balance < 0) // If the balance is negative, apply an overdraft fee
         {
             cout << " Account balance went negative. Overdraft fee of $25.00 being applied" << endl;
         }
-        else if (accountbalance > 0)
+        else if (account_balance > 0) // If the balance is positive, no fee is applied
         {
             cout << " No overdraft fee applied" << endl;
         }
     }
-    void apply_overdraft_fee() // apples the over draft fee to the users account
+
+    /**
+     * Method to apply the overdraft fee to the account balance.
+     * Deducts a fixed overdraft fee from the current balance.
+     */
+    void apply_overdraft_fee()
     {
-        accountbalance -= overdraftfee;
+        account_balance -= overdraft_fee; // Deduct the overdraft fee from the account balance
         cout << " === Current Account Balance === " << endl;
-        get_current_balance();
+        cout << "Current Balance: $" << getBalance() << "\n" << endl; // Display the updated balance
     }
 
 private:
-    double overdraft_limit;
-    double interest = 0.02;
-    double overdraftfee = 25.00;
+    double overdraft_limit{}; // Limit for overdrafts on the account
+    double interest = 0.02; // Interest rate for the checking account
+    double overdraft_fee = 25.00; // Fee for overdrawing the account
 };
--- CheckingAccount.h
+++ CheckingAccount.h
@@ -1,16 +1,40 @@
-//
-// Created by Austin Tesch on 12/4/24.
-//
-
 #ifndef CHECKINGACCOUNT_H
 #define CHECKINGACCOUNT_H
 
+#include "BankAccount.h"
+#include <string>
 
+/**
+ * CheckingAccount class represents a checking account derived from the base BankAccount class.
+ * It provides additional functionality specific to checking accounts, such as overdraft checks and fees.
+ */
+class CheckingAccount : public BankAccount
+{
+public:
+    double account_balance; // Current balance of the checking account
 
-class CheckingAccount {
+    /**
+     * Constructor to initialize the checking account with account details.
+     */
+    CheckingAccount(int accountNumber, const std::string &holderName, double initialBalance);
 
+    /**
+     * Method to check if the account balance has gone negative.
+     * If the balance is negative, an overdraft fee will be applied.
+     * If the balance is positive, no fee is applied.
+     */
+    void check_overdraft();
+
+    /**
+     * Method to apply the overdraft fee to the account balance.
+     * Deducts a fixed overdraft fee from the current balance.
+     */
+    void apply_overdraft_fee();
+
+private:
+    double overdraft_limit; // Limit for overdrafts on the account
+    double interest = 0.02; // Interest rate for the checking account
+    double overdraft_fee = 25.00; // Fee for overdrawing the account
 };
 
-
-
-#endif //CHECKINGACCOUNT_H
+#endif // CHECKINGACCOUNT_H
