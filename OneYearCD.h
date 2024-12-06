5ef08632d9e66ff2c42cb5972691db4d18c21634
Tayler <taylercaufield@gmail.com>
2024-12-05 19:37:10 -0800

    Update OneYearCD.h

--- OneYearCD.h
+++ OneYearCD.h
@@ -1,9 +1,10 @@
 #ifndef One_Year_CD_H 
 #define One_Year_CD_H 
 
-/**              
- * Class for handling the Three Year CD operations
- */
+#include "CertificateOfDepositbase.h"
+#include "BankAccount.h"
+
+// class for handling One Year CD operations 
 class OneYearCD
 {
 public:
@@ -11,15 +12,11 @@ time_t duration;
 double apy;
 double penalty; 
     OneYearCD() = default;
-/*
-function that applies interest to the customers account 
-*/
 
+// function that applies interest to the customers account 
 void applyInterest(double apy, time_t duration); 
 
-/*
-function that applies a penalty to the customers account 
-*/
+// function that applies a penalty to the customers account 
 
 void applyPenalty(double balance, double penalty); 
 // function for calculating the penalty for the customer 
