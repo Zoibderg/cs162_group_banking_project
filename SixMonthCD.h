c4db2bc2da2d0d958aa7db45b345692f1fd021c4
Tayler <taylercaufield@gmail.com>
2024-12-05 19:36:17 -0800

    Update SixMonthCD.h

--- SixMonthCD.h
+++ SixMonthCD.h
@@ -1,33 +1,32 @@
 #ifndef Six_Month_CD_H 
 #define Six_Month_CD_H 
 
-/**              
- * Class for handling the Six Year CD operations
- */
+#include "CertificateOfDepositbase.h"
+#include "BankAccount.h"
+
+// class for handling Six Month CD operations
 class SixMonthCD
 {
 public:
-    /**
-     * Default constructor
-     */
-    SixMonthCD() = default;
+  // defauly constructor 
+SixMonthCD() = default;
 
 time_t duration;
 double apy;
 double penalty; 
-/*
-function for applying interest to customers account
-*/
+
+// function for applying interest to customers account
+
  void applyInterest(double apy, time_t duration); 
 // function to calculate the interest for the customer
  double calculateInterest();  
-/*
-function for applying penalty to customers account
-*/ 
+
+// function for applying penalty to customers account
+
  void applyPenalty(double balance, double penalty); 
 // function to calculate the penatly for the customer 
  double calculatePenalty(); 
-
+// deconstructor 
  ~SixMonthCD();
 };
 
