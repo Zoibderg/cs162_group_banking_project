820debe12638be5f1f4fa086ad6eb4be63e3903d
Tayler <taylercaufield@gmail.com>
2024-12-05 18:57:49 -0800

    CD classes

---
+++ ThreeMonthCD.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "CertificateOfDepositBase.h"
+
+class ThreeMonthCD : public BankAccount
+{
+public:
+	/*
+	shows how long the duration of the Cd is
+	*/
+	time_t duration = 3;
+	/*
+	shows the apy for the specific Cd duration
+	*/
+	double apy = 0.025;
+	/*
+	the penalty for the specifc Cd duration
+	*/
+	double penalty = 0.1;
+	/*
+	calculates the penalty using the customers current balance
+	and the penatly percentage for the CD
+	*/
+	double calculatePentaly()
+	{
+		double finalpenalty = balance * penalty;
+	}
+	/*
+	calculates the interest earned based on the apy and duration
+	for that CD
+	*/
+	double calculateInterest()
+	{
+		double interest = apy * duration;
+	}
+	/*
+	applies the penalty to the users account
+	*/
+	void applyPenalty()
+	{
+		cout << "Your penatly is: " << finalpenalty << endl;
+		balance += finalpenalty
+	}
+	/*
+	applies the interest to the users account
+	*/
+	void applyInterest()
+	{
+		cout << " Your interest is: " << interest << endl;
+		balance += interest;
+	}
+
+};
