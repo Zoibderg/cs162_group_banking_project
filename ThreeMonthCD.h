1f54ed973aec270d7ba6c3b064df08f71c2524d4
Tayler <taylercaufield@gmail.com>
2024-12-05 19:35:05 -0800

    Update ThreeMonthCD.h

--- ThreeMonthCD.h
+++ ThreeMonthCD.h
@@ -1,19 +1,18 @@
 #ifndef Three_Month_CD_H 
 #define Three_Month_CD_H 
 
-/**              
- * Class for handling the Three Year CD operations
- */
+#include "CertificateOfDepositbase.h"
+#include "BankAccount.h"
+
+// class for handling Three Month CD operations 
 class ThreeMonthCD
 {
 public:
 time_t duration;
 double apy;
 double penalty; 
-    /**
-     * Default constructor
-     */
-    ThreeMonthCD() = default;
+// defaul constructor
+ThreeMonthCD() = default;
 // function for adding the interest to the customers account
  void applyInterest(double apy, time_t duration); 
 // function for adding the penalty to the customers account 
