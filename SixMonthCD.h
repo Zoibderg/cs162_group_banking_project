36b151d8e248b5f61f955989612b0d7f3037ee8a
Tayler <taylercaufield@gmail.com>
2024-12-05 19:30:45 -0800

    Update SixMonthCD.h

--- SixMonthCD.h
+++ SixMonthCD.h
@@ -19,12 +19,13 @@ double penalty;
 function for applying interest to customers account
 */
  void applyInterest(double apy, time_t duration); 
+// function to calculate the interest for the customer
  double calculateInterest();  
 /*
 function for applying penalty to customers account
 */ 
  void applyPenalty(double balance, double penalty); 
-
+// function to calculate the penatly for the customer 
  double calculatePenalty(); 
 
  ~SixMonthCD();
